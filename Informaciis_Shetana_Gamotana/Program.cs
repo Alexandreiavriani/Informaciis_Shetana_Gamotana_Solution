c1391a03fe1384f0d48f949ed0cdef01f5954c84
Alexandre Iavriani <aleksandre.iavrian@gau.edu.ge>
2021-05-17 19:21:22 +0400

    Add project files.

---
+++ Informaciis_Shetana_Gamotana/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File_Stream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            string path = @"F:\SomeDir2";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+
+           
+
+            using (Stream fStream = new FileStream($"{path}\\note.txt", FileMode.OpenOrCreate))
+            {
+
+                byte[] bytes = new byte[] { 95, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100 };
+                //{'H','e','l','l','o',' ','W','o','r','l','d'}.
+                fStream.Write(bytes, 0, bytes.Length);
+
+                fStream.WriteByte(33); //=!
+
+                Console.WriteLine("Text is write");
+            } 
+
+
+            
+            using (Stream fStream = File.OpenRead($"{path}\\note.txt"))
+            {
+                
+                byte[] byteS = new byte[fStream.Length];
+
+                fStream.Read(byteS, 0, byteS.Length);
+                
+                string textFromFile = Encoding.Default.GetString(byteS);
+                Console.WriteLine($"Text in file: {textFromFile}");
+            }
+
+             
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MemoryStream/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Memory_Stream
+{
+    class MemoryStreamDemo
+    {
+        static void Main()
+        {
+            
+            MemoryStream memoryStream = new MemoryStream(100);
+
+           
+
+            byte[] giorgi = Encoding.Default.GetBytes("Giorgi"); 
+            byte[] giorgadze = Encoding.Default.GetBytes("Giorgadze");  
+
+           
+            memoryStream.Write(giorgi, 0, giorgi.Length);
+            memoryStream.Write(giorgadze, 0, giorgadze.Length);
+
+           
+            Console.WriteLine("Capacity: {0} , Length: {1}",
+                                  memoryStream.Capacity.ToString(),
+                                  memoryStream.Length.ToString());
+
+            
+            Console.WriteLine("Position: " + memoryStream.Position);
+
+           
+            memoryStream.Seek(-9, SeekOrigin.Current);
+
+           
+            Console.WriteLine("Position: " + memoryStream.Position);
+
+           
+            byte[] vsBytes = Encoding.Default.GetBytes(" and ");
+            
+            memoryStream.Write(vsBytes, 0, vsBytes.Length);
+
+
+        
+
+            string data = Encoding.Default.GetString(memoryStream.GetBuffer());
+
+          
+            Console.WriteLine(data);
+
+            Console.WriteLine();
+            Console.WriteLine("Finish!");
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+
+}
